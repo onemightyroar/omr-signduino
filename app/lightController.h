bddaf051e9b23a6235c577fbd45577104db39ec6
Trevor Suarez <rican7@gmail.com>
2013-07-03 15:35:23 -0400

    New basic controller for controlling the LED light strip

---
+++ app/lightController.h
@@ -0,0 +1,21 @@
+// Declare our scope's strip
+Adafruit_NeoPixel strip = NULL;
+
+// Allow us to set our strip in this controller from another scope
+void setLightStrip(Adafruit_NeoPixel &lightStrip) {
+	strip = lightStrip;
+}
+
+// Change the color of ALL of the strip's LEDs
+void changeColorAll(uint8_t R, uint8_t G, uint8_t B) {
+	// Create our color from our RGB values
+	uint32_t color = strip.Color(R, G, B);
+
+	// Loop through each pixel in our strip
+	for (uint16_t i = 0; i < strip.numPixels(); i++) {
+		strip.setPixelColor(i, color);
+	}
+
+	// Show the things!
+	strip.show();
+}
