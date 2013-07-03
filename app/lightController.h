40c4de643c294fcd74a24479e75e3cce940e13cb
Trevor Suarez <rican7@gmail.com>
2013-07-03 17:24:35 -0400

    New initializer and retabbing for the lightController

--- app/lightController.h
+++ app/lightController.h
@@ -3,19 +3,27 @@ Adafruit_NeoPixel strip = NULL;
 
 // Allow us to set our strip in this controller from another scope
 void setLightStrip(Adafruit_NeoPixel &lightStrip) {
-	strip = lightStrip;
+    strip = lightStrip;
+}
+
+void initLightStrip(Adafruit_NeoPixel &lightStrip) {
+    // Set our scope's light strip
+    setLightStrip(lightStrip);
+
+    // Clear the light strip back to "black/blank"
+    lightStrip.show();
 }
 
 // Change the color of ALL of the strip's LEDs
 void changeColorAll(uint8_t R, uint8_t G, uint8_t B) {
-	// Create our color from our RGB values
-	uint32_t color = strip.Color(R, G, B);
+    // Create our color from our RGB values
+    uint32_t color = strip.Color(R, G, B);
 
-	// Loop through each pixel in our strip
-	for (uint16_t i = 0; i < strip.numPixels(); i++) {
-		strip.setPixelColor(i, color);
-	}
+    // Loop through each pixel in our strip
+    for (uint16_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, color);
+    }
 
-	// Show the things!
-	strip.show();
+    // Show the things!
+    strip.show();
 }
